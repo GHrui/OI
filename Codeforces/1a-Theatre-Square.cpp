be1bc4c015ac9735054f0a774910a077983aa88b
Grui <836162880@qq.com>
2017-10-08 14:28:36 +0800

    AC

--- Codeforces/1a-Theatre-Square.cpp
+++ Codeforces/1a-Theatre-Square.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 
 int main(void){
-    unsigned long long int a, m, n;
+    unsigned long long int a, m, n, ans;
     std::cin >> n >> m >> a;
-    unsigned long long int ans = ((n % a == 0 ? (n / a) : (n / a + 1)) * (m % a == 0 ? (m / a) : (m / a + 1)));
+    ans = ((n % a == 0 ? (n / a) : (n / a + 1)) * (m % a == 0 ? (m / a) : (m / a + 1)));
     std::cout << ans << "\n";
     return 0;
 }
